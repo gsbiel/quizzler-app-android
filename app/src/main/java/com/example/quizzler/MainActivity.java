2bdd539ede51bd63ba5d1cbdf1c554bfb20515c9
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-04-20 19:51:29 -0300

    implement QuizzlerBrain and Question data models, following MVC architecture

--- app/src/main/java/com/example/quizzler/MainActivity.java
+++ app/src/main/java/com/example/quizzler/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.quizzler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView scoreLabel;
+    private TextView questionLabel;
+    private ProgressBar progressBar;
+    private Button firstAnswerBtn;
+    private Button secondAnswerBtn;
+    private QuizzlerBrain quizzlerBrain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.quizzlerBrain = new QuizzlerBrain();
+
+        this.catchReferencesFromLayout();
+        this.registerForEvents();
+
+        this.updateUI();
+    }
+
+    private void catchReferencesFromLayout() {
+        this.scoreLabel = findViewById(R.id.scoreLabel);
+        this.firstAnswerBtn = findViewById(R.id.firstAnswer);
+        this.secondAnswerBtn = findViewById(R.id.secondAnswer);
+        this.questionLabel = findViewById(R.id.questionLabel);
+        this.progressBar = findViewById(R.id.progressBar);
+    }
+
+    private void registerForEvents() {
+
+        this.firstAnswerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        this.secondAnswerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void updateUI(){
+        this.questionLabel.setText(this.quizzlerBrain.getCurrentQuestion());
+        this.progressBar.setProgress((int) this.quizzlerBrain.getProgress());
     }
 }
---
+++ app/src/main/java/com/example/quizzler/QuizzlerBrain.java
@@ -0,0 +1,53 @@
+package com.example.quizzler;
+
+public class QuizzlerBrain {
+
+    private Question[] questions = {
+            new Question("A slug's blood is green.", true),
+            new Question("Approximately one quarter of human bones are in the feet.", true),
+            new Question("The total surface area of two human lungs is approximately 70 square metres.", true),
+            new Question("In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", true),
+            new Question("In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", false),
+            new Question("It is illegal to pee in the Ocean in Portugal.", true),
+            new Question("You can lead a cow down stairs but not up stairs.", false),
+            new Question("Google was originally called 'Backrub'.", true),
+            new Question("Buzz Aldrin's mother's maiden name was 'Moon'.", true),
+            new Question("The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", false),
+            new Question("No piece of square dry paper can be folded in half more than 7 times.", false),
+            new Question("Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", true)
+    };
+
+    private int currentQuestion;
+
+    private double progress;
+
+    private int score;
+
+    QuizzlerBrain(){
+        this.currentQuestion = 0;
+        this.score = 0;
+        this.progress = ((float) this.currentQuestion / (float) this.questions.length) + 10;
+    }
+
+    public String nextQuestion(){
+        this.currentQuestion += 1;
+        return this.getCurrentQuestion();
+    }
+
+    public String getCurrentQuestion(){
+        return this.questions[this.currentQuestion].getQuestion();
+    }
+
+    public double getProgress(){
+        return this.progress;
+    }
+
+    public Boolean checkAnswer(Boolean answer){
+        Boolean isCorrect = this.questions[this.currentQuestion].validateAnswer(answer);
+        if(isCorrect){
+            this.score += 10;
+        }
+        return isCorrect;
+    }
+
+}
