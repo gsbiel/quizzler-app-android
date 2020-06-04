2eaffbd5fb5c133368e6a81ef494ddb85c9ca51f
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-06-04 15:03:12 -0300

    create Question data class and implement companion object compounded by dummy questions in QuizzlerViewModel

--- app/src/main/java/com/example/quizzler/quizzler/QuizzlerViewModel.kt
+++ app/src/main/java/com/example/quizzler/quizzler/QuizzlerViewModel.kt
@@ -6,11 +6,24 @@ import androidx.lifecycle.ViewModel
 
 class QuizzlerViewModel: ViewModel() {
 
-//    companion object{
-//        private val questionBank = listOf(
-//
-//        )
-//    }
+    private data class Question(val question: String, val answer: Boolean)
+
+    companion object{
+        private val questionBank = listOf(
+                Question("A slug's blood is green.", true),
+                Question("Approximately one quarter of human bones are in the feet.", true),
+                Question("The total surface area of two human lungs is approximately 70 square metres.", true),
+                Question("In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", true),
+                Question("In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", false),
+                Question("It is illegal to pee in the Ocean in Portugal.", true),
+                Question("You can lead a cow down stairs but not up stairs.", false),
+                Question("Google was originally called 'Backrub'.", true),
+                Question("Buzz Aldrin's mother's maiden name was 'Moon'.", true),
+                Question("The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", false),
+                Question("No piece of square dry paper can be folded in half more than 7 times.", false),
+                Question("Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", true)
+        )
+    }
 
     private val _score = MutableLiveData<Int>()
     val score: LiveData<Int>
